2f84102e0a7fc47e7b6d1703a1b019c65b2513c5
Victor Henrique <victorhenriqu3@outlook.com>
2022-02-09 00:27:18 -0400

    Adicionando o TS e implementando o Express

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response, NextFunction } from "express";
+
+const app = express();
+
+app.get("/status", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ foo: "ba" });
+});
+
+app.listen(3000, () => {
+  console.log("Aplicação executando na porta 3000!");
+});

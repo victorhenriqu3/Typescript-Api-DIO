e69eee298128efc42d619e89063f6a44c2688e5b
Victor Henrique Monteiro Lima <victorhenriqu3@outlook.com>
2022-02-10 14:50:32 -0400

    Adicionando Get e Get por uuid

---
+++ src/routes/users.route.ts
@@ -0,0 +1,19 @@
+import StatusCodes from "http-status-codes";
+import { Router, Request, Response, NextFunction } from "express";
+
+const usersRoute = Router();
+
+usersRoute.get("/users", (req: Request, res: Response, next: NextFunction) => {
+  const users = [{ userName: "Victor" }];
+  res.status(StatusCodes.OK).send(users);
+});
+
+usersRoute.get(
+  "/users/:uuid",
+  (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    res.status(StatusCodes.OK).send({ uuid });
+  }
+);
+
+export default usersRoute;

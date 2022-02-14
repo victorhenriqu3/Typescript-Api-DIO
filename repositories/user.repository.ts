7544d936e3c4c28a0a5211117ffd7453b5f8f840
Victor Henrique Monteiro Lima <victorhenriqu3@outlook.com>
2022-02-14 14:44:17 -0400

    adicionando Listagem por Id

--- repositories/user.repository.ts
+++ repositories/user.repository.ts
@@ -4,9 +4,19 @@ import User from "../src/models/user.model";
 class UserRepository {
   async findAllUsers(): Promise<User[]> {
     const sql = `SELECT uuid,username FROM application_user`;
-    const res = await db.query<User>(sql);
-    const rows = res.rows;
+    const { rows } = await db.query<User>(sql);
+
     return rows || [];
+  }
+
+  async findByUuid(uuid: string): Promise<User> {
+    const values = [uuid];
+    const sql = `SELECT uuid,username FROM application_user WHERE uuid = $1`;
+
+    const { rows } = await db.query<User>(sql, values);
+    const [user] = rows;
+
+    return user;
   }
 }
 
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -14,9 +14,11 @@ usersRoute.get(
 
 usersRoute.get(
   "/users/:uuid",
-  (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+  async (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
-    res.status(StatusCodes.OK).send({ uuid });
+    const user = await userRepository.findByUuid(uuid);
+
+    res.status(StatusCodes.OK).send(user);
   }
 );
 
